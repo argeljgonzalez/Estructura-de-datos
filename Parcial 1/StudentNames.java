2fe777db988a71f81215729f246c4bead69f083e
Argel González <argel.j.gonzalez@gmail.com>
2025-02-05 10:22:53 -0600

    third commit

--- Parcial 1/StudentNames.java
+++ Parcial 1/StudentNames.java
@@ -1,3 +1,16 @@
+/**
+ * Autor: Argel Jhamir Gonzalez Ontiveros
+ * Matrícula: 14571
+ * Fecha: 04-Feb-2025
+ */
+
+   /**
+ * Muestra una lista de todos los nombres de un salon.
+ * @param showStudentsNames es la funcion donde se ralizara el show de la lista.
+ * @param studentsNamesArray es la variable donde guarda el arrgelo con todos los nombres de los mismos.
+ * @return los nombres de todos se muestran y te muestra el total del mismo con @param .length
+ */
+
 class StudentsNames {
     public void showStudentsNames() {
         
