d6442c5934e11a1c3a38ac743f70403f88df00aa
Argel González <argel.j.gonzalez@gmail.com>
2025-02-06 10:47:47 -0600

    commit number 7

--- Parcial 1/UserData.java
+++ Parcial 1/UserData.java
@@ -1,3 +1,9 @@
+ /**
+ * Autor: Argel Jhamir Gonzalez Ontiveros
+ * Matrícula: 14571
+ * Fecha: 06-Feb-2025
+ */
+
 public class UserData {
     
     public String fullName(String firstName, String LastName){
